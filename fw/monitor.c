46a728a7920b776ac477b53a2c446384201c570b
Aleksander Kaminski <aleksander.kaminski@phoenix-rtos.com>
2022-09-01 23:01:10 +0200

    Add monitor addtional features menu

--- fw/monitor.c
+++ fw/monitor.c
@@ -4,6 +4,7 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 #include <6502.h>
 #include <pocket265.h>
 
@@ -12,6 +13,97 @@
 #include "memory.h"
 
 typedef enum { mode_data = 0, mode_addr } edit_mode_t;
+
+extern uint16_t user_memsz;
+
+void monitor_memcpy(void)
+{
+	dl1414_puts("\nNot implemented");
+	keyboard_get();
+}
+
+void monitor_i2c_save(void)
+{
+	dl1414_puts("\nNot implemented");
+	keyboard_get();
+}
+
+void monitor_i2c_load(void)
+{
+	dl1414_puts("\nNot implemented");
+	keyboard_get();
+}
+
+void monitor_cpuid(void)
+{
+	const char *str;
+	unsigned char cpu = getcpu();
+
+	switch (cpu) {
+	case CPU_6502:    str = "\n6502";    break;
+	case CPU_65C02:   str = "\n65C02";   break;
+	case CPU_65816:   str = "\n65816";   break;
+	case CPU_4510:    str = "\n4510";    break;
+	case CPU_65SC02:  str = "\n65SC02";  break;
+	case CPU_65CE02:  str = "\n65CE02";  break;
+	case CPU_HUC6280: str = "\nHUC6280"; break;
+	case CPU_2A0x:    str = "\n2A0x";    break;
+	case CPU_45GS02:  str = "\n45GS02";  break;
+	default:          str = "\nUnknown"; break;
+	}
+
+	dl1414_puts(str);
+	keyboard_get();
+}
+
+void monitor_memclr(void)
+{
+	memset((void *)USR_MEM_START, 0, user_memsz);
+	dl1414_puts("\nMemclr done");
+	keyboard_get();
+}
+
+void monitor_menu(void)
+{
+	unsigned char selection = 0;
+	char buff[SCREEN_LENGTH + 2];
+	unsigned char key, exit = 0;
+	static const struct {
+		const char *name;
+		void (*f)(void);
+	} items[] = {
+		{ "Copy mem", monitor_memcpy },
+		{ "I2C save", monitor_i2c_save },
+		{ "I2C load", monitor_i2c_load },
+		{ "CPU id", monitor_cpuid },
+		{ "Clear mem", monitor_memclr }
+	};
+
+	while (!exit) {
+		sprintf(buff, "\n%d:%s", selection + 1, items[selection]);
+		dl1414_puts(buff);
+
+		key = keyboard_get();
+
+		switch (key) {
+		case KEY_INC:
+			if (++selection >= sizeof(items) / sizeof(items[0]))
+				selection = 0;
+			break;
+		case KEY_DEC:
+			if (selection == 0)
+				selection = (sizeof(items) / sizeof(items[0]));
+			--selection;
+			break;
+		case KEY_GO:
+			items[selection].f();
+			break;
+		case KEY_SEL:
+			exit = 1;
+			break;
+		}
+	}
+}
 
 void monitor_run(void)
 {
@@ -77,6 +169,9 @@ void monitor_run(void)
 				break;
 
 			case KEY_F2:
+				monitor_menu();
+				break;
+
 			case KEY_F3:
 			case KEY_F4:
 				/* TODO */
