3c3a4c2a81046ecf8d026a4751a60253a2a0c71f
Aleksander Kaminski <aleksander.kaminski@phoenix-rtos.com>
2022-09-05 11:29:04 +0200

    Improved gpio implementation (fight against lack of optimalisation from cc65)

--- fw/gpio.c
+++ fw/gpio.c
@@ -8,28 +8,22 @@ unsigned char g_gpio_output;
 
 void gpio_setPin(char pin, char state)
 {
-	volatile unsigned char *ptr = (void *)GPIO_BASE;
-
 	if (state)
 		g_gpio_output |= (1 << pin);
 	else
 		g_gpio_output &= ~(1 << pin);
 
-	*ptr = g_gpio_output;
+	*((volatile unsigned char *)GPIO_BASE) = g_gpio_output;
 }
 
 char gpio_getPin(char pin)
 {
-	volatile unsigned char *ptr = (void *)GPIO_BASE;
-
-	return !!(*ptr & (1 << pin));
+	return (*((volatile unsigned char *)GPIO_BASE) & (1 << pin)) ? 1 : 0;
 }
 
 void gpio_init(void)
 {
-	volatile unsigned char *ptr = (void *)GPIO_BASE;
-
-	*ptr = 0xf;
+	*((volatile unsigned char *)GPIO_BASE) = 0xf;
 	g_gpio_output = 0xf;
 }
 
